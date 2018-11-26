00a7e35d29e01f4a266b736e5ff863bd492bef64
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-11-25 22:59:19 -0200

    Flash messages

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -8,15 +8,17 @@ class SessionController {
   async store (req, res) {
     const { email, password } = req.body
 
+    console.log(req.body)
+
     const user = await User.findOne({ where: { email } })
 
     if (!user) {
-      console.log('Usuário não encontrado')
+      req.flash('error', 'Usuário não encontrado')
       return res.redirect('/')
     }
 
     if (!await user.checkPassword(password)) {
-      console.log('Senha incorreta')
+      req.flash('error', 'Senha incorreta')
       return res.redirect('/')
     }
 
